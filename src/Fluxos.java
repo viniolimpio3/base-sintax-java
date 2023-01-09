4e57cc96a4babdb6a0f8dcd406a91f5472fc4fca
Vinícius Olímpio <viniolimpio3@gmail.com>
2023-01-09 20:01:18 -0300

    Laços

--- src/Fluxos.java
+++ src/Fluxos.java
@@ -11,6 +11,25 @@ public class Fluxos {
         } else
             System.out.println("Você é menor de idade");
 
+        int count = 0;
+        int total = 0;
+        while(count <= 10){
+            total += count;
+            System.out.println(count);
+            count++;
+        }
+        System.out.println(total);
+
+        //Laços encadeados:
+        for(int linha = 0; linha < 5; linha++) {
+            for (int coluna = 0; coluna < 5; coluna++) {
+                if ( coluna > linha ) {
+                    break;
+                }
+                System.out.print( coluna + 1 );
+            }
+            System.out.println();
+        }
 
     }
 }
